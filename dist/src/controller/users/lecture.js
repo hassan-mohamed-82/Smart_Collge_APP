7075953204bd03f42749efc742517f51e5d5a190
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-12-30 13:25:41 +0200

    update lecture

--- dist/src/controller/users/lecture.js
+++ dist/src/controller/users/lecture.js
@@ -2,33 +2,51 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getLectureById = exports.getLectures = void 0;
 const lecture_1 = require("../../models/shema/lecture");
-const BadRequest_1 = require("../../Errors/BadRequest");
-const response_1 = require("../../utils/response");
 const Errors_1 = require("../../Errors");
-const Errors_2 = require("../../Errors");
+const response_1 = require("../../utils/response");
+const BadRequest_1 = require("../../Errors/BadRequest");
 const getLectures = async (req, res) => {
-    const level = req.query.level;
-    const department = req.query.department;
-    if (!req.user)
-        throw new Errors_2.UnauthorizedError("Not authorized to access this route");
-    const UserId = req.user.id;
-    const lectures = await lecture_1.LectureModel.find({ level, department });
-    if (!lectures)
+    if (!req.user) {
+        throw new Errors_1.UnauthorizedError("Not authorized to access this route");
+    }
+    const { level, department } = req.query;
+    // Build query object dynamically
+    const query = {};
+    if (level) {
+        query.level = Number(level); // Convert string to number
+    }
+    if (department) {
+        query.department = department;
+    }
+    const lectures = await lecture_1.LectureModel.find(query);
+    if (lectures.length === 0) {
         throw new Errors_1.NotFound("No lectures found");
+    }
     (0, response_1.SuccessResponse)(res, lectures);
 };
 exports.getLectures = getLectures;
 const getLectureById = async (req, res) => {
-    const level = req.query.level;
-    const department = req.query.department;
-    if (!req.user)
-        throw new Errors_2.UnauthorizedError("Not authorized to access this route");
-    const id = req.params.id;
-    if (!id)
+    if (!req.user) {
+        throw new Errors_1.UnauthorizedError("Not authorized to access this route");
+    }
+    const { id } = req.params;
+    const { level, department } = req.query;
+    if (!id) {
         throw new BadRequest_1.BadRequest("Id is required");
-    const lecture = await lecture_1.LectureModel.findById({ _id: id, level, department });
-    if (!lecture)
+    }
+    // Build query
+    const query = { _id: id };
+    if (level) {
+        query.level = Number(level);
+    }
+    if (department) {
+        query.department = department;
+    }
+    // Use findOne instead of findById for multiple conditions
+    const lecture = await lecture_1.LectureModel.findOne(query);
+    if (!lecture) {
         throw new Errors_1.NotFound("No lecture found");
+    }
     (0, response_1.SuccessResponse)(res, lecture);
 };
 exports.getLectureById = getLectureById;
--- src/controller/users/lecture.ts
+++ src/controller/users/lecture.ts
@@ -1,36 +1,63 @@
 import { LectureModel } from "../../models/shema/lecture";
-import { saveBase64Image } from "../../utils/handleImages";
-import { BadRequest } from "../../Errors/BadRequest";
+import {  NotFound, UnauthorizedError } from "../../Errors";
 import { SuccessResponse } from "../../utils/response";
-import { NotFound } from "../../Errors";
-import { UnauthorizedError } from "../../Errors";
 import { Request, Response } from "express";
+import { BadRequest } from "../../Errors/BadRequest";
 
 export const getLectures = async (req: Request, res: Response) => {
-    const level = req.query.level;
-    const department = req.query.department;
- if(!req.user)
- throw new UnauthorizedError("Not authorized to access this route")
- const UserId = req.user.id;
-    const lectures = await LectureModel.find({level,department});
-    if(!lectures)
-    throw new NotFound("No lectures found")
-SuccessResponse(res,lectures)
-}
+    if (!req.user) {
+        throw new UnauthorizedError("Not authorized to access this route");
+    }
 
+    const { level, department } = req.query;
+
+    // Build query object dynamically
+    const query: Record<string, any> = {};
+    
+    if (level) {
+        query.level = Number(level); // Convert string to number
+    }
+    if (department) {
+        query.department = department;
+    }
+
+    const lectures = await LectureModel.find(query);
+    
+    if (lectures.length === 0) {
+        throw new NotFound("No lectures found");
+    }
+
+    SuccessResponse(res, lectures);
+};
 
 export const getLectureById = async (req: Request, res: Response) => {
-    const level = req.query.level;
-    const department = req.query.department;
-    if(!req.user)
-    throw new UnauthorizedError("Not authorized to access this route")
-    const id = req.params.id;
-    if(!id)
-    throw new BadRequest("Id is required")
-    const lecture = await LectureModel.findById({_id:id,level,department});
-    if(!lecture)
-    throw new NotFound("No lecture found")
-SuccessResponse(res,lecture)
-}
+    if (!req.user) {
+        throw new UnauthorizedError("Not authorized to access this route");
+    }
 
+    const { id } = req.params;
+    const { level, department } = req.query;
 
+    if (!id) {
+        throw new BadRequest("Id is required");
+    }
+
+    // Build query
+    const query: Record<string, any> = { _id: id };
+    
+    if (level) {
+        query.level = Number(level);
+    }
+    if (department) {
+        query.department = department;
+    }
+
+    // Use findOne instead of findById for multiple conditions
+    const lecture = await LectureModel.findOne(query);
+
+    if (!lecture) {
+        throw new NotFound("No lecture found");
+    }
+
+    SuccessResponse(res, lecture);
+};
