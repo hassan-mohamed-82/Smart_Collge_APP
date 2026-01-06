4e5b2173c85978c335f50a285b15287a696b83c1
hs301443 <106682004+hs301443@users.noreply.github.com>
2026-01-06 10:00:07 +0200

    update again schema

--- dist/src/models/shema/templates.js
+++ dist/src/models/shema/templates.js
@@ -38,7 +38,7 @@ const mongoose_1 = __importStar(require("mongoose"));
 exports.TemplateSchema = new mongoose_1.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
-    category: { type: String, enum: ['Training', 'Diploma', ' Masters', 'Doctorate'], required: true },
+    category: { type: String, enum: ['Training', 'Diploma', 'Masters', 'Doctorate'], required: true },
     startdate: { type: Date, default: Date.now },
     enddate: { type: Date },
     Image: { type: String },
--- src/models/shema/templates.ts
+++ src/models/shema/templates.ts
@@ -5,7 +5,7 @@ export const TemplateSchema = new Schema({
 
     title: { type: String, required: true },
     content: { type: String, required: true },
-    category: { type: String, enum: ['Training', 'Diploma', ' Masters','Doctorate'], required: true },
+    category: { type: String, enum: ['Training', 'Diploma', 'Masters','Doctorate'], required: true },
     startdate: { type: Date, default: Date.now },
     enddate: { type: Date },
     Image: { type: String },
